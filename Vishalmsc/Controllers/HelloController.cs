e5a9747be17872a321cb10fff1c4b6cf72ca2bcb
Vk-githhub17 <84117838+Vk-githhub17@users.noreply.github.com>
2023-05-08 08:57:30 +0530

    Add project files.

---
+++ Vishalmsc/Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vishalmsc.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content ("Vishal is a good boy");
+        }
+    }
+}
+ 
